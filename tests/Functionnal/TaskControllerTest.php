2874eed6c46d26e562d629244432859d03d05c83
HervillardLeo <136316086+HervillardLeo@users.noreply.github.com>
2025-02-13 18:47:44 +0100

    test: :white_check_mark: add functionnal test for add task

--- tests/Functionnal/TaskControllerTest.php
+++ tests/Functionnal/TaskControllerTest.php
@@ -2,15 +2,19 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
     private $client;
+    private $entityManager;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
+        $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
     }
 
     public function testListTasks(): void
@@ -19,5 +23,18 @@ class TaskControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testAddTask(): void
+    {
+        $this->client->request('POST', '/tasks', [], [], ['CONTENT_TYPE' => 'application/json'], json_encode([
+            'title' => 'New Task'
+        ]));
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertJson($this->client->getResponse()->getContent());
+
+        $newTask = $this->entityManager->getRepository(Task::class)->findOneBy(['title'=> 'New Task']);
+        $this->assertNotNull($newTask);
     }
 }
